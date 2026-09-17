31766d25107ac6268968b7112f0f3eb617f9d849
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-09-18 00:57:24 +0530

    feat(selection): mount selection provider

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,13 +1,16 @@
 import { AppProvider } from './context/AppProvider';
 import { CandidateProvider } from '../features/candidates/context/CandidateProvider';
 import { InterviewProvider } from '../features/interviews/context/InterviewProvider';
+import { SelectionProvider } from '../features/selection/context/SelectionProvider';
 import { AppContent } from './AppContent';
 
 export const App = () => (
   <AppProvider>
     <CandidateProvider>
       <InterviewProvider>
-        <AppContent />
+        <SelectionProvider>
+          <AppContent />
+        </SelectionProvider>
       </InterviewProvider>
     </CandidateProvider>
   </AppProvider>
