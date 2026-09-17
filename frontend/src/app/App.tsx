cd3302d90e8da55e79a07a6ec8c7b000c11e1ca2
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-09-18 00:28:08 +0530

    feat(interviews): register interview provider in app

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,14 @@
 import { AppProvider } from './context/AppProvider';
 import { CandidateProvider } from '../features/candidates/context/CandidateProvider';
+import { InterviewProvider } from '../features/interviews/context/InterviewProvider';
 import { AppContent } from './AppContent';
 
 export const App = () => (
   <AppProvider>
     <CandidateProvider>
-      <AppContent />
+      <InterviewProvider>
+        <AppContent />
+      </InterviewProvider>
     </CandidateProvider>
   </AppProvider>
 );
