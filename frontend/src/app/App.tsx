38dc1652e666399a77975311a501821f6d4eca95
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-09-18 11:23:50 +0530

    feat: register authentication and notifications providers

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,16 +2,22 @@ import { AppProvider } from './context/AppProvider';
 import { CandidateProvider } from '../features/candidates/context/CandidateProvider';
 import { InterviewProvider } from '../features/interviews/context/InterviewProvider';
 import { SelectionProvider } from '../features/selection/context/SelectionProvider';
+import { AuthProvider } from '../features/auth/context/AuthProvider';
+import { NotificationProvider } from '../features/notifications/context/NotificationProvider';
 import { AppContent } from './AppContent';
 
 export const App = () => (
-  <AppProvider>
-    <CandidateProvider>
-      <InterviewProvider>
-        <SelectionProvider>
-          <AppContent />
-        </SelectionProvider>
-      </InterviewProvider>
-    </CandidateProvider>
-  </AppProvider>
+  <AuthProvider>
+    <NotificationProvider>
+      <AppProvider>
+        <CandidateProvider>
+          <InterviewProvider>
+            <SelectionProvider>
+              <AppContent />
+            </SelectionProvider>
+          </InterviewProvider>
+        </CandidateProvider>
+      </AppProvider>
+    </NotificationProvider>
+  </AuthProvider>
 );
